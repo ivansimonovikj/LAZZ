00acfc8c3c54e3eb85581c59e01f0b623a6082bb
Ivan Simonovikj <ivansimonovik@gmail.com>
2025-12-15 05:00:56 +0100

    Add menu icon click event to open menu link

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const instaDiv = document.getElementById("instaIcon");
+const menuDiv = document.getElementById("menuIcon");
 
 instaDiv.addEventListener("click", () => {
   window.open("https://www.instagram.com/cafe_de_laz/", "_blank");
+});
+
+menuDiv.addEventListener("click", () => {
+  window.open("https://cafedelazmenu.netlify.app/", "_blank");
 });
